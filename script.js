3d8226bbb715485e900e803118a95ae9b745e0df
Ryan <137423730+Ryanpereirabergamasco@users.noreply.github.com>
2025-06-12 12:05:16 -0300

    Add files via upload
    Versão-1.0

---
+++ script.js
@@ -0,0 +1,15 @@
+function abrirMenu() {
+  document.getElementById("menuLateral").style.width = "250px";
+}
+
+function fecharMenu() {
+  document.getElementById("menuLateral").style.width = "0";
+}
+
+function abrirMenuNotficacao() {
+  document.getElementById("menuLateralnotificacao").style.width = "250px";
+}
+
+function fecharMenuNotficacao() {
+  document.getElementById("menuLateralnotificacao").style.width = "0";
+}
